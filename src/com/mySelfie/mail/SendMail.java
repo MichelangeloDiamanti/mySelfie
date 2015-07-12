bebc8d66633db8562a7c86e1710da16bef39b20e
Michelangelo Diamanti <michelangelo.diamanti@gmail.com>
2015-07-12 16:46:00 +0200

    [Sensitive info. in context] ora le credenziali della mail vengono prese dal contesto invece che essere harcoded.
    vanno aggiunte queste due righe nel context.xml
    
        <Environment name="myselfieConfirmationEmail" value="myselfieconfirmation@gmail.com" type="java.lang.String"/>
        <Environment name="myselfieConfirmationPassword" value="<changeMe>" type="java.lang.String"/>

--- src/com/mySelfie/mail/SendMail.java
+++ src/com/mySelfie/mail/SendMail.java
@@ -11,13 +11,15 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 public class SendMail {
 	
-	// credenziali account confirmation
-    private final static String username = "myselfieconfirmation@gmail.com";
-    private final static String password = "idiDFEwFLGKb";
-	
+//    private final static String username = "myselfieconfirmation@gmail.com";
+//    private final static String password = "idiDFEwFLGKb";
+    
     /**
      * prende in input la mail dello user a cui mandare il messaggio di conferma 
      * e, dopo aver creato un token random e averlo incorporato in un link manda
@@ -28,60 +30,77 @@ public class SendMail {
      * @return							il token generato se tutto è andato bene altrimenti null
      */
     public static String sendSignupConfirmation(String recipientUsername, String recipientEmailAddress) {
-    	
-    	String token = null;
-    	
-    	// impostazioni server mail google
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-        
-        // apre una sessione SMTP con autenticazione
-        Session session = Session.getInstance(props,
-          new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-          });
 
-        // manda il messaggio
-        try {
-        	
-        	// genera un numero random
-        	SecureRandom random = new SecureRandom();
-        	/*
-        	 *  genera una stringa randomica di lunghezza 160/5 = 32
-        	 *  160 --> è il numero in input di BigInteger rappresenta il numero di bits
-        	 *  		casuali da cui pescare
-        	 *  5 	--> è 2^5 il numero di bit passati in input al metodo toString
-        	 */
-        	token = new BigInteger(160, random).toString(32);
             
-        	// dichiara un nuovo messaggio mime
-            Message message = new MimeMessage(session);
-            // imposta il sender
-            message.setFrom(new InternetAddress("myselfieconfirmation@gmail.com"));
-            // imposta il destinatario
-            message.setRecipients(Message.RecipientType.TO,
-                InternetAddress.parse(recipientEmailAddress));
-            // imposta l'oggetto della mail
-            message.setSubject("Myselfie - Confirm Account");
-            // imposta il testo della mail
-            message.setText(
-            		"Dear "
-            		+ recipientUsername
-            		+ "\n\nPlease follow this link to confirm your account: "
-            		+ "https://localhost:8443/mySelfie/validation/"
-            		+ token
-            		);
-            // manda il messaggio
-            Transport.send(message);
+    	InitialContext initialContext;	// necessario a leggere le credenziali dal context
+    	String token = null;			// token random verifica mail
+    	
+		try {
+			
+			/*
+			 * prende le credenziali di accesso della email dal file context.xml
+			 */
+			initialContext = new InitialContext();
+	    	Context environmentContext = (Context) initialContext.lookup("java:/comp/env");
+	    	final String email = (String) environmentContext.lookup("myselfieConfirmationEmail");
+	    	final String password = (String) environmentContext.lookup("myselfieConfirmationPassword");
+	    	
+	    	// impostazioni server mail google
+	        Properties props = new Properties();
+	        props.put("mail.smtp.auth", "true");
+	        props.put("mail.smtp.starttls.enable", "true");
+	        props.put("mail.smtp.host", "smtp.gmail.com");
+	        props.put("mail.smtp.port", "587");
+	        
+	        // apre una sessione SMTP con autenticazione
+	        Session session = Session.getInstance(props,
+	          new javax.mail.Authenticator() {
+	            protected PasswordAuthentication getPasswordAuthentication() {
+	                return new PasswordAuthentication(email, password);
+	            }
+	          });
 
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+	        // manda il messaggio
+	        try {
+	        	
+	        	// genera un numero random
+	        	SecureRandom random = new SecureRandom();
+	        	/*
+	        	 *  genera una stringa randomica di lunghezza 160/5 = 32
+	        	 *  160 --> è il numero in input di BigInteger rappresenta il numero di bits
+	        	 *  		casuali da cui pescare
+	        	 *  5 	--> è 2^5 il numero di bit passati in input al metodo toString
+	        	 */
+	        	token = new BigInteger(160, random).toString(32);
+	            
+	        	// dichiara un nuovo messaggio mime
+	            Message message = new MimeMessage(session);
+	            // imposta il sender
+	            message.setFrom(new InternetAddress("myselfieconfirmation@gmail.com"));
+	            // imposta il destinatario
+	            message.setRecipients(Message.RecipientType.TO,
+	                InternetAddress.parse(recipientEmailAddress));
+	            // imposta l'oggetto della mail
+	            message.setSubject("Myselfie - Confirm Account");
+	            // imposta il testo della mail
+	            message.setText(
+	            		"Dear "
+	            		+ recipientUsername
+	            		+ "\n\nPlease follow this link to confirm your account: "
+	            		+ "https://localhost:8443/mySelfie/validation/"
+	            		+ token
+	            		);
+	            // manda il messaggio
+	            Transport.send(message);
+
+	        } catch (MessagingException e) {
+	            throw new RuntimeException(e);
+	        }
+		} catch (NamingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	
         // ritorna il token generato
         return token;
     }
